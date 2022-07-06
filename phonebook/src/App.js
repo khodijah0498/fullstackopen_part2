e4b99c41b477463884ee645851d875b72c2f23d6
khodijah0498 <khodijahlawal@gmail.com>
2022-07-06 11:24:18 +0100

    2.1: Course information step6

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  const Header = () => {
+    return(
+  <h1>{course.name}</h1>
+  )
+  }
+  const Part = (props) => {
+    return(
+      <p>{props.name} {props.exercises}</p>
+    )
+  }
+  const Content = () => {
+    return (
+      <><Part name={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+       <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
+      
+      </>
+      
+    );
+  }
+  
+  const mapItems = course.parts.map(part => part.id)
+  console.log(mapItems)
+
+  const Course = () => {
+    return(<div>
+      <Header/>
+      <Content/>
+  {/* // <h1>{course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}</h1> */}
     </div>
-  );
-}
+    )
+  }
+// return <div>"Hello world"</div>
+  return <Course course={course} />;
+};
 
 export default App;
